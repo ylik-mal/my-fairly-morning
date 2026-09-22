7bd9c413c72b22457acc8bdbac959ef85ae80cf6
ylik-mal <malkovaju08@gmail.com>
2026-09-22 23:52:46 +0400

    Add base project files and folders

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не найден в .env файле")
